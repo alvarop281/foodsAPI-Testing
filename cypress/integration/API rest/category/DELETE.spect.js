f0ffc2ad302e2a3322747f8c130f985fbea25750
Alvaro Pacheco <47091614+alvarop281@users.noreply.github.com>
2022-01-02 18:43:32 -0400

    All the test cases were created, for the DELETE method of the category resource.

---
+++ cypress/integration/API rest/category/DELETE.spect.js
@@ -0,0 +1,134 @@
+var faker = require('faker');
+const testUser = Cypress.env('env').testUser;
+const testNonAdminUser = Cypress.env('env').testNonAdminUser;
+var newCategory;
+
+describe('Test method put for category resource', () => {
+
+    it('create a category with the correct properties', () => {
+        cy.request({
+                method: 'POST',
+                url: 'categories/',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'auth-token': testUser.token
+                },
+                body: {
+                    description: faker.fake("{{lorem.word}}"),
+                    icon: faker.fake("{{image.avatar}}")
+                },
+            })
+            .then((response) => {
+                // check when all correct properties are sent, status code must be 201
+                expect(response.status).to.eq(201)
+                    // check when all correct properties are sent, success property must be true
+                expect(response.body).to.have.property('success', true)
+                    // check when all correct properties are sent, businessMessage property must have a message
+                expect(response.body.businessMessage.message).to.exist
+                    // check when all correct properties are sent, objects property must have a category
+                expect(response.body.objects.category).to.exist
+                    // check when all correct properties are sent, category property must have a id
+                expect(response.body.objects.category.id).to.exist
+                    // check when all correct properties are sent, category property must have a description
+                expect(response.body.objects.category.description).to.exist
+                    // check when all correct properties are sent, category property must have a icon
+                expect(response.body.objects.category.icon).to.exist
+
+                newCategory = response.body.objects.category;
+            })
+    })
+
+    // Verify if request has a wrong token
+    it('DELETE - header has a wrong token', () => {
+        cy.request({
+                method: 'DELETE',
+                url: 'categories/' + newCategory.id,
+                headers: {
+                    'Content-Type': 'application/json',
+                    'auth-token': 'Incorrect'
+                },
+                failOnStatusCode: false
+            })
+            .then((response) => {
+                // status code must be 401
+                expect(response.status).to.eq(401)
+                    // success property must be false
+                expect(response.body).to.have.property('success', false)
+                    // businessError.param property must to exist
+                expect(response.body.businessError.param).to.exist
+                    // businessError.param property must to be "token"
+                expect(response.body.businessError.param).to.eq("token")
+                    // objects property length must be 0
+                expect(response.body.objects).to.have.length(0)
+            })
+    })
+
+    // Verify if request has a token of a non-administrator user
+    it('DELETE - header has a token of a non-administrator user', () => {
+        cy.request({
+                method: 'DELETE',
+                url: 'categories/' + newCategory.id,
+                headers: {
+                    'Content-Type': 'application/json',
+                    'auth-token': testNonAdminUser.token
+                },
+                failOnStatusCode: false
+            })
+            .then((response) => {
+                // status code must be 401
+                expect(response.status).to.eq(401)
+                    // success property must be false
+                expect(response.body).to.have.property('success', false)
+                    // businessError.param property must to exist
+                expect(response.body.businessError.param).to.exist
+                    // businessError.param property must to be "token"
+                expect(response.body.businessError.param).to.eq("User")
+                    // objects property length must be 0
+                expect(response.body.objects).to.have.length(0)
+            })
+    })
+
+    // Verify if request has wrong id property
+    it('DELETE - body has wrong id property', () => {
+        cy.request({
+                method: 'DELETE',
+                url: 'categories/' + 9999999,
+                headers: {
+                    'Content-Type': 'application/json',
+                    'auth-token': testUser.token
+                },
+                failOnStatusCode: false
+            })
+            .then((response) => {
+                //Verify if request has id property, status code must be 400
+                expect(response.status).to.eq(401)
+                    // Verify if request has id property, success property must be false
+                expect(response.body).to.have.property('success', false)
+                    // Verify if request has id property, businessError.param property is equal to id
+                expect(response.body.businessError.param).to.eq("id")
+                    // Verify if request has id property, objects property length must be 0
+                expect(response.body.objects).to.have.length(0)
+            })
+    })
+
+    // check when all correct properties are sent
+    it('DELETE - delete a category', () => {
+        cy.request({
+                method: 'DELETE',
+                url: 'categories/' + newCategory.id,
+                headers: {
+                    'Content-Type': 'application/json',
+                    'auth-token': testUser.token
+                }
+            })
+            .then((response) => {
+                // check when all correct properties are sent, status code must be 200
+                expect(response.status).to.eq(200)
+                    // check when all correct properties are sent, success property must be true
+                expect(response.body).to.have.property('success', true)
+                    // check when all correct properties are sent, businessMessage property must have a message
+                expect(response.body.businessMessage.message).to.exist
+            })
+    })
+
+})
